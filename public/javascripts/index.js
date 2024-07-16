38d2522b660e073c03fbbae8b737b22a2dfa4fc1
Tomi Helin <tomi.helin@student.lut.fi>
2024-07-16 20:47:37 +0300

    index page changes

---
+++ public/javascripts/index.js
@@ -0,0 +1,31 @@
+if (document.readyState !== "loading") {
+    console.log("Document is ready");
+    initializeCode();
+} else {
+    document.addEventListener("DOMContentLoaded", function() {
+        console.log("Document ready after waiting!");
+        initializeCode();
+    })
+}
+
+function initializeCode() {
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      const contentDiv = document.getElementById('content');
+      contentDiv.innerHTML = `
+        <p>Welcome, ${payload.email}</p>
+        <button id="logout">Logout</button>
+      `;
+      
+      document.getElementById('logout').addEventListener('click', () => {
+        localStorage.removeItem('auth_token');
+        location.reload();
+      });
+    } else {
+      const contentDiv = document.getElementById('content');
+      contentDiv.innerHTML = `
+        <p>Please <a href="/register.html">Register</a> or <a href="/login.html">Login</a></p>
+      `;
+    }
+}
