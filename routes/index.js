03d3815126662aa84099fbdaadf49365b5fb4c7f
Tomi Helin <tomi.helin@student.lut.fi>
2024-07-16 19:57:46 +0300

    Added route for login.html

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/register.html', function(req, res, next) {
   res.render('register', {title: 'Register'});
 });
 
+router.get('/login.html', function(req, res, next) {
+  res.render('login', { title: "Login"});
+});
+
 module.exports = router;
